9c8db6b90ad3bef7a469763b4247da303095a918
treilly6 <treilly6@binghamton.edu>
2020-02-05 09:34:39 -0500

    working on date formats for saving and querying db for meals

--- src/Components/MealJournal/JournalHome.js
+++ src/Components/MealJournal/JournalHome.js
@@ -10,18 +10,24 @@ export default function JournalHome(props) {
 
     console.log("HERE THE JOURNAL HOME PROPS ", props);
 
+    const [journalDate, setJournalDate] = useState(null);
+
     // props.location.state will have a redirectDate value when a user saves
     // meal data on the FoodItem.js page and it redirects back to the journalHome
     // this will maintain a consistency with the date so users could add multiple meal items
     // for the same date relatively quickly
     // If there is not redirectDate value then set the dateObj to the current date
-    const dateObj = (props.location.state && props.location.state.redirectDate ? new Date(props.location.state.redirectDate) : new Date())
 
-    const [journalDate, setJournalDate] = useState(dateObj);
+    // inital render set the state for the journal date
+    useEffect(() => {
+        console.log("USE EFFECT IN JOURNAL HOME ");
+        const dateObj = (props.location.state && props.location.state.redirectDate ? new Date(props.location.state.redirectDate) : new Date());
+        setJournalDate(dateObj)
+    }, []);
 
-    const getDate = (date) => {
-        console.log("HERE IS THE DATE IN THE JOURNAL HOME ", date);
-        setJournalDate(date);
+    useEffect(() => {
+        console.log("USE EFFECT FOR SENDING THE AXIOS TO FOOD ITEMS ")
+        console.log("HERET HE JDATE , " , journalDate);
 
         // want an api call when the date changes to get the proper data for that days meals and calories
         axios.get('/api/foodItems' , {params : {date : journalDate}})
@@ -32,6 +38,18 @@ export default function JournalHome(props) {
             .catch(err => {
                 console.log(err);
             })
+
+    }, [journalDate])
+
+
+
+
+
+
+    const getDate = (date) => {
+        console.log("HERE IS THE DATE IN THE JOURNAL HOME ", date);
+        console.log("HERE THE JDATE BEFOER STATE CHANGE ", journalDate);
+        setJournalDate(date);
     }
 
     return (
