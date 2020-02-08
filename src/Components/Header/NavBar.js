2774896f246fd0b65790982551332665aa98a0cf
treilly6 <treilly6@binghamton.edu>
2020-02-08 14:04:53 -0500

    worked on nav menu and drop down

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -8,10 +8,13 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { UserContext } from '../../context/UserContext';
 import axios from "axios";
+import NavDropDown from './NavDropDown';
+import './Header.css';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+    position : "relative",
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -25,8 +28,8 @@ export default function NavBar() {
   const classes = useStyles();
   const { user, setUser } = useContext(UserContext);
 
-  console.log("NAV BAR IMPORTANT SHIT HERE");
-  console.log(user);
+  // initalize the dropdown ref as state
+  const [navDown, setNavDown] = useState(false);
 
   const userLogOut = () => {
       console.log("Loigout function");
@@ -41,30 +44,37 @@ export default function NavBar() {
         .catch(err => console.log(err))
   }
 
+  // changes the nav down state variable which is passed to the dropdown menu
+  const toggleMenu = () => {
+      // change the state of the nav drop down status
+      // (true would mean dropdown is down and false is it is hidden/up)
+      setNavDown(!navDown);
+  }
+
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton onClick={toggleMenu} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" className={classes.title}>
-            News
+            Calorie Counter
           </Typography>
           {user ? (
-              <React.Fragment>
+              <div className="flex-col">
                   <div>{user.username}</div>
                   <Button onClick={() => userLogOut()} color="inherit">Log out</Button>
-              </React.Fragment>
+              </div>
           ) : (
-              <React.Fragment>
+              <div className="flex-col">
                   <Button onClick={() => {window.location = "/login"}} color="inherit">Log in</Button>
                   <Button onClick={() => {window.location = "/signup"}} color="inherit">Sign up</Button>
-              </React.Fragment>
+              </div>
           )}
-
         </Toolbar>
       </AppBar>
+      <NavDropDown navDown={navDown} setNavDown={setNavDown} />
     </div>
   );
 }
