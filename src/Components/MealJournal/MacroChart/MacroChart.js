78253d61e59134e06104a24999305c7a3d22b55e
treilly6 <treilly6@binghamton.edu>
2020-02-13 09:33:58 -0500

    style format for pie chart completed

--- src/Components/MealJournal/MacroChart/MacroChart.js
+++ src/Components/MealJournal/MacroChart/MacroChart.js
@@ -1,17 +1,33 @@
 import React from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Legend } from 'recharts';
 
-const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
-];
+
+// Legend for the Pie chart
+const PieLegend = () => {
+    return (
+        <div>
+            <div className="legend-elem-cont">
+                <div className="legend-elem">
+                    <div className="legend-label">Fats</div>
+                    <div className="box" style={{backgroundColor : "#b8502e"}}></div>
+                </div>
+                <div className="legend-elem">
+                    <div className="legend-label">Carbs</div>
+                    <div className="box" style={{backgroundColor : "#73b82e"}}></div>
+                </div>
+                <div className="legend-elem">
+                    <div className="legend-label">Protein</div>
+                    <div className="box" style={{backgroundColor : "#2e95b8"}}></div>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 const dataTest = [
-    { name : "fats", calories : 300, "% cals" : "30%" },
-    { name : "carbs", calories : 500,"% cals" : "50%"},
-    { name : "protein", calories : 200,"% cals" : "20%"}
+    { name : "fats", calories : 300},
+    { name : "carbs", calories : 500},
+    { name : "protein", calories : 200}
 ];
 
 const colorMap = {
@@ -36,24 +52,31 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, per
 export default function MacroChart() {
 
     return (
-        <div style={{display : "flex", justifyContent : "center"}}>
-            <PieChart width={400} height={400} >
-                <Pie
-                data={dataTest}
-                cx={200}
-                cy={200}
-                labelLine={false}
-                label={renderCustomizedLabel}
-                outerRadius={100}
-                fill="#8884d8"
-                dataKey="calories"
-                >
-                    {
-                      dataTest.map((entry, index) => <Cell key={`cell-${index}`} fill={colorMap[entry.name]} />)
-                    }
-                </Pie>
-            </PieChart>
+        <div style={{position : "relative"}}>
+            <div style={{position : "absolute", top : "5px", fontSize : "1.15em", fontWeight : "bold", textAlign : "center", width : "100%"}}>Calorie Breakdown</div>
+            <div style={{display : "flex", justifyContent : "center", flexDirection : "column" , margin : "10px 0px"}}>
+                <ResponsiveContainer width="100%" height={250}>
+                    <PieChart>
+                        <Pie
+                        data={dataTest}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius="75%"
+                        fill="#8884d8"
+                        dataKey="calories"
+                        >
+                            {
+                              dataTest.map((entry, index) => <Cell key={`cell-${index}`} fill={colorMap[entry.name]} />)
+                            }
+                        </Pie>
+                    </PieChart>
+                </ResponsiveContainer>
+                <PieLegend />
+            </div>
         </div>
+
     );
 
 }
