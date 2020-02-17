d7fccbcceaebe2ff1e08ed72fd77701b78c746cb
treilly6 <treilly6@binghamton.edu>
2020-02-17 10:01:13 -0500

    changed the logout redirect and the landing page header

--- src/Components/Header/NavBar.js
+++ src/Components/Header/NavBar.js
@@ -7,6 +7,7 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { UserContext } from '../../context/UserContext';
+import { Redirect } from 'react-router-dom';
 import axios from "axios";
 import NavDropDown from './NavDropDown';
 import './Header.css';
@@ -25,11 +26,20 @@ const useStyles = makeStyles(theme => ({
 }));
 
 export default function NavBar() {
+
   const classes = useStyles();
   const { user, setUser } = useContext(UserContext);
 
   // initalize the dropdown ref as state
   const [navDown, setNavDown] = useState(false);
+
+  const [redirect, setRedirect] = useState(null);
+
+  const currentPath = () => {
+      console.log("Here is the current url");
+      console.log(window.location.pathname);
+      return window.location.pathname;
+  }
 
   const userLogOut = () => {
       console.log("Loigout function");
@@ -38,11 +48,15 @@ export default function NavBar() {
             console.log("Result for the logout here");
             console.log(res.data);
             if(res.data.success) {
-                setUser(null);
+                // redirect to the homepage
+                window.location = res.data.redirect;
             }
         })
         .catch(err => console.log(err))
   }
+
+  // get the url of the current page
+  const urlPathname = currentPath();
 
   // changes the nav down state variable which is passed to the dropdown menu
   const toggleMenu = () => {
@@ -51,30 +65,63 @@ export default function NavBar() {
       setNavDown(!navDown);
   }
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton onClick={toggleMenu} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Calorie Counter
-          </Typography>
-          {user ? (
-              <div className="flex-col">
-                  <div style={{textAlign : "center"}}>{user.username}</div>
-                  <Button onClick={() => userLogOut()} color="inherit">Log out</Button>
-              </div>
-          ) : (
-              <div className="flex-col">
-                  <Button onClick={() => {window.location = "/login"}} color="inherit">Log in</Button>
-                  <Button onClick={() => {window.location = "/signup"}} color="inherit">Sign up</Button>
-              </div>
-          )}
-        </Toolbar>
-      </AppBar>
-      <NavDropDown navDown={navDown} setNavDown={setNavDown} />
-    </div>
-  );
+  console.log("HERE IS THE URL BEFORE THE RETURN ", urlPathname);
+
+  if(urlPathname === "/") {
+      return (
+        <div className={classes.root} style={{position : "absolute", zIndex : 5, width:"100%"}}>
+          <AppBar position="static" style={{backgroundColor : "transparent", boxShadow : "none"}}>
+            <Toolbar>
+              <IconButton onClick={toggleMenu} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" className={classes.title}>
+                Calorie Counter
+              </Typography>
+              {user ? (
+                  <div className="flex-col">
+                      <div style={{textAlign : "center"}}>{user.username}</div>
+                      <Button onClick={() => userLogOut()} color="inherit">Log out</Button>
+                  </div>
+              ) : (
+                  <div className="flex-col">
+                      <Button onClick={() => {window.location = "/login"}} color="inherit">Log in</Button>
+                      <Button onClick={() => {window.location = "/signup"}} color="inherit">Sign up</Button>
+                  </div>
+              )}
+            </Toolbar>
+          </AppBar>
+          <NavDropDown navDown={navDown} setNavDown={setNavDown} />
+        </div>
+      );
+  } else {
+      return (
+          <div className={classes.root}>
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton onClick={toggleMenu} edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                  Calorie Counter
+                </Typography>
+                {user ? (
+                    <div className="flex-col">
+                        <div style={{textAlign : "center"}}>{user.username}</div>
+                        <Button onClick={() => userLogOut()} color="inherit">Log out</Button>
+                    </div>
+                ) : (
+                    <div className="flex-col">
+                        <Button onClick={() => {window.location = "/login"}} color="inherit">Log in</Button>
+                        <Button onClick={() => {window.location = "/signup"}} color="inherit">Sign up</Button>
+                    </div>
+                )}
+              </Toolbar>
+            </AppBar>
+            <NavDropDown navDown={navDown} setNavDown={setNavDown} />
+          </div>
+        );
+  }
+
+
 }
